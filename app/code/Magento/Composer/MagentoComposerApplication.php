40521ad62b1330db113c886388d589010a11a52d
Ivan Gavryshko <igavryshko@ebay.com>
2015-06-23 18:33:07 -0500

    MAGETWO-38838: Create composer management library and reuse it in both updater and setup wizard
    - added method to create composer object

--- app/code/Magento/Composer/MagentoComposerApplication.php
+++ app/code/Magento/Composer/MagentoComposerApplication.php
@@ -7,19 +7,43 @@
 namespace Magento\Composer;
 
 use Composer\Console\Application;
+use Composer\IO\BufferIO;
+use Composer\Factory as ComposerFactory;
 
 class MagentoComposerApplication
 {
+    private $configIsSet = false;
+
+    private $composerHome;
+
+    private $composerJson;
+
     public function __construct(
         Application $consoleApplication = null
     ) {
         $this->consoleApplication = $consoleApplication ? $consoleApplication : new Application();
-        $this->consoleApplication->setAutoExit(false);
     }
 
     public function setConfig($pathToComposerHome, $pathToComposerJson)
     {
+        $this->composerJson = $pathToComposerJson;
+        $this->composerHome = $pathToComposerHome;
+
         putenv('COMPOSER_HOME=' . $pathToComposerHome);
         putenv('COMPOSER=' . $pathToComposerJson);
+
+        $this->consoleApplication->setAutoExit(false);
+        $this->configIsSet = true;
+
+    }
+
+    public function getComposer()
+    {
+        if (!$this->configIsSet) {
+            throw new \Exception('Please call setConfig method to set config');
+        }
+
+        return ComposerFactory::create(new BufferIO(), $this->composerJson);
+
     }
 }
