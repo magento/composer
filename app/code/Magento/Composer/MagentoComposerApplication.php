0d767f0ff64802a01d3484e3bc960448244f148b
Ivan Gavryshko <igavryshko@ebay.com>
2015-06-23 15:58:53 -0500

    MAGETWO-38838: Create composer management library and reuse it in both updater and setup wizard
    - added composer.json

---
+++ app/code/Magento/Composer/MagentoComposerApplication.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Composer;
+
+use Composer\Console\Application;
+
+class MagentoComposerApplication
+{
+    public function __construct(
+        $pathToComposerJson,
+        $pathToComposerHome,
+        Application $consoleApplication = null
+    ) {
+        $this->consoleApplication = $consoleApplication ? $consoleApplication : new Application();
+        $this->consoleApplication->setAutoExit(false);
+        putenv('COMPOSER_HOME=' . $pathToComposerHome);
+        putenv('COMPOSER=' . $pathToComposerJson);
+
+    }
+}
